46bf9b3206a1a276db3dc4e6d7da2314ba20ea6e
Pierre Guillou <pierre.guillou@lip6.fr>
2024-05-27 23:48:19 +0200

    Range for loops for enums

--- src/Core/db.cpp
+++ src/Core/db.cpp
@@ -1315,10 +1315,9 @@ void db_locators_correct(VectorString& strings,
 
   /* Loop on the reference locator */
 
-  auto it = ELoc::getIterator();
-  while (it.hasNext())
+  for(auto el : ECov::iter())
   {
-    if (*it != ELoc::UNKNOWN)
+    if (el != ELoc::UNKNOWN)
     {
       /* Store the ranks of the locators matching the reference locator
        */
@@ -1326,14 +1325,13 @@ void db_locators_correct(VectorString& strings,
       for (int i = 0; i < number; i++)
       {
         if (locatorIdentify(strings[i], &cur_type, &cur_item, &nmult)) continue;
-        if (cur_type != *it) continue;
+        if (cur_type != el) continue;
         rank[nmatch++] = cur_item;
       }
       // Do not forget to increment the iterator!
       // 'continue' keyword should be forbidden!!
       if (nmatch <= 0)
       {
-        it.toNext();
         continue;
       }
 
@@ -1350,7 +1348,7 @@ void db_locators_correct(VectorString& strings,
       for (int i = 0; i < number; i++)
       {
         if (locatorIdentify(strings[i], &cur_type, &cur_item, &nmult)) continue;
-        if (cur_type != *it) continue;
+        if (cur_type != el) continue;
         found = -1;
         for (int k = 0; k < ncount && found < 0; k++)
           if (ind[k] == nmatch) found = k;
@@ -1358,7 +1356,6 @@ void db_locators_correct(VectorString& strings,
         nmatch++;
       }
     }
-    it.toNext();
   }
 }
 
