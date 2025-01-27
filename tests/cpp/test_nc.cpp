d0e5b90f7a91a015592dd48e38888f3f5b2d45d4
Pierre Guillou <pierre.guillou@minesparis.psl.eu>
2025-01-27 14:32:51 +0100

    Extend test

--- tests/cpp/test_nc.cpp
+++ tests/cpp/test_nc.cpp
@@ -1,3 +1,6 @@
+#include "Basic/Timer.hpp"
+#include <Db/DbGrid.hpp>
+
 #include <ncFile.h>
 #include <ncDim.h>
 #include <ncVar.h>
@@ -65,9 +68,29 @@ int run(const std::array<int, 3>& dims)
   return 0;
 }
 
+int test(const std::array<int, 3>& dims)
+{
+  auto db = std::unique_ptr<DbGrid>(DbGrid::create(VectorInt {dims.begin(), dims.end()}));
+  {
+    Timer tm;
+    db->dumpToNF("dbgrid.nf");
+    auto db2 = std::unique_ptr<DbGrid>(DbGrid::createFromNF("dbgrid.nf"));
+    db2->dumpToNF("dbgrid2.nf");
+    tm.displayIntervalMilliseconds();
+  }
+  {
+    Timer tm;
+    db->dumpToNC("dbgrid.nc");
+    auto db2 = std::unique_ptr<DbGrid>(DbGrid::createFromNC("dbgrid.nc"));
+    db2->dumpToNC("dbgrid2.nc");
+    tm.displayIntervalMilliseconds();
+  }
+  return 0;
+}
+
 int main()
 {
-  std::array<int, 3> dims {10, 10, 10};
-  run(dims);
+  std::array<int, 3> dims {200, 200, 200};
+  test(dims);
   return 0;
 }
