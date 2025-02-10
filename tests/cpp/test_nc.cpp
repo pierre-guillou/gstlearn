609e48c83ac13e24687991b051e9ca122ad41093
Pierre Guillou <pierre.guillou@minesparis.psl.eu>
2025-02-10 20:45:38 +0100

    [test] More error handling

--- tests/cpp/test_nc.cpp
+++ tests/cpp/test_nc.cpp
@@ -10,14 +10,28 @@ int test(const std::array<int, 3>& dims)
     Timer tm;
     db->dumpToNF("dbgrid.nf");
     auto db2 = std::unique_ptr<DbGrid>(DbGrid::createFromNF("dbgrid.nf"));
-    db2->dumpToNF("dbgrid2.nf");
+    if (db2 != nullptr)
+    {
+      db2->dumpToNF("dbgrid2.nf");
+    }
+    else
+    {
+      messerr("Cannot deserialize `dbgrid.nf'");
+    }
     tm.displayIntervalMilliseconds();
   }
   {
     Timer tm;
     db->dumpToNC("dbgrid.nc");
     auto db2 = std::unique_ptr<DbGrid>(DbGrid::createFromNC("dbgrid.nc"));
-    db2->dumpToNC("dbgrid2.nc");
+    if (db2 != nullptr)
+    {
+      db2->dumpToNC("dbgrid2.nc");
+    }
+    else
+    {
+      messerr("Cannot deserialize `dbgrid.nc'");
+    }
     tm.displayIntervalMilliseconds();
   }
   return 0;
