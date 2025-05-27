c99c5fc999ba35a9d75c6e5d803bf0108b4552df
Pierre Guillou <pierre.guillou@minesparis.psl.eu>
2025-05-27 09:33:27 +0200

    Revert "fix script"
    This reverts commit 29cd178c8e6efa8026c862c754f45620a1d1968d.

--- python/run_test_py.py
+++ python/run_test_py.py
@@ -3,6 +3,7 @@ import sys
 import os
 
 # This script retrieve the standard output of a python test script (argv[1]) and print it into a log file saved in the given directory (argv[2])
+python_exe = os.path.realpath(sys.executable)
 test_script = sys.argv[1]
 out_dir = sys.argv[2]
 test_name = os.path.splitext(os.path.basename(test_script))[0] # No extension
@@ -10,7 +11,7 @@ test_output = os.path.join(out_dir, test_name + ".out")
 
 # Retrieve all standard outputs (C, python, etc..) and print to log file
 # Use unbuffered (-u) output for "printing" on the fly
-str_output = subprocess.check_output(["uv", "run", "python", "-u", test_script], encoding='utf-8')
+str_output = subprocess.check_output([python_exe, "-u", test_script], encoding='utf-8')
 with open(test_output, "w+", encoding='utf8') as output:
   output.write(str_output)
 
