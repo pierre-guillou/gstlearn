4894ceb9e91511dadacba6ca844c697c4437307f
Pierre Guillou <pierre.guillou@minesparis.psl.eu>
2025-11-04 09:43:35 +0100

    vario

--- src/Core/model_auto.cpp
+++ src/Core/model_auto.cpp
@@ -4495,6 +4495,8 @@ Id model_auto_fit(Vario* vario,
   /* Load the arrays */
 
   VH::dump("wt0", RECINT.wt);
+  vario->display();
+  std::cout << mauto.getWmode() << " " << npadir << "\n";  
   st_load_wt(vario, mauto.getWmode(), npadir, RECINT.wt);
   VH::dump("wt", RECINT.wt);
   st_compress_array(vario, npadir, RECINT.wt, RECINT.wtc);
